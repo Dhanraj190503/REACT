c2c4007cdaa8006914bb384bae20d4b1555888b3
Dhanraj190503 <dhanrajghadi1234@gmail.com>
2025-05-12 20:31:19 +0530

    comments for ThemeChanger

--- ThemeChanger/src/App.jsx
+++ ThemeChanger/src/App.jsx
@@ -7,6 +7,8 @@ import Card from './Components/Card';
 function App() {
 
   const [themeMode, setThemeMode] = useState('light');
+  // here themeMode is taken from the ThemeContext and setThemeMode is used to set the themeMode.
+  // useState is used to create a state variable and setState is used to update the state variable.
 
   const LightTheme = () =>{
     setThemeMode('light');
@@ -14,17 +16,20 @@ function App() {
   const DarkTheme = () =>{
     setThemeMode('dark');
   }
+  // here LightTheme and DarkTheme are used to set the themeMode to light and dark respectively.
+  // LightTheme and DarkTheme are passed as props to the ThemeProvider.
 
   // Acutal change in Theme
-
   useEffect(() => {
     document.querySelector('html').classList.remove('light','dark');
 
     document.querySelector('html').classList.add(themeMode);
   }, [themeMode]);
+//  useEffect is used to perform side effects in function components.
 
   return (
     <ThemeProvider value={{themeMode, DarkTheme, LightTheme}}>
+      {/* ThemeProvider is used to provide the context to the components. */}
 
        <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
--- ThemeChanger/src/Context/Theme.js
+++ ThemeChanger/src/Context/Theme.js
@@ -5,9 +5,13 @@ export const ThemeContext = createContext({
     DarkTheme: () =>{},
     LightTheme: () =>{},
 });
+// createContext is used to create a context object.
 
 export const ThemeProvider = ThemeContext.Provider;
+// ThemeProvider is used to provide the context to the components.
 
 export default function useTheme(){
     return useContext(ThemeContext)
 }
+// useTheme is a custom hook that allows us to use the context in our components.
+// useContext is used to consume the context in the components.
